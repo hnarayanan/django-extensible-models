0b25d437feb8b401fa817e6bacada2525ce1db37
Harish Narayanan <hnarayanan@gmail.com>
2022-12-18 12:32:53 +0000

    Add the notion of a schema for the extra field

--- src/crm/base_models.py
+++ src/crm/base_models.py
@@ -1,7 +1,10 @@
 import uuid
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.sites.models import Site
+
 
 from django_extensions.db.models import TitleSlugDescriptionModel, ActivatorModel
 
@@ -11,11 +14,28 @@ class BaseModel(TitleSlugDescriptionModel, ActivatorModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    extra = models.JSONField(null=True, blank=True)
+    extra = models.JSONField(default=dict)
 
     portals = models.ManyToManyField(Site)
+    # modified_by =
 
     class Meta:
         abstract = True
 
-    # modified_by =
+
+class ExtraFieldSchema(models.Model):
+
+    schema = models.JSONField() # Should be valid JSON Schema
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.schema
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+
+    portals = models.ManyToManyField(Site)
